722c1b68c71e507c8ed9b4d4e571eafe03b7206f
Popeydanielmate <14269@coderacademy.edu.au>
2024-07-06 14:00:55 +1000

    feat: error handling middleware in place

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 
-const express = require ("express");
+const express = require("express");
 const app = express();
+
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.json({
-        message:"Hello World"
-    });
+app.get("/", (request, response, next) => {
+
+	return next(new Error("Error on purpose from root route"))
+
+	response.json({
+		message:"Hello world!"
+	});
 });
+
+app.use((error, request, response, next) => {
+	response.json({
+		message:"Error occured!",
+		error: error
+	});
+});
+
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
