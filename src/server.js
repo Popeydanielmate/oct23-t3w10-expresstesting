2b3b3e35f86cce82eff6c47151b8c053a0c91734
Popeydanielmate <14269@coderacademy.edu.au>
2024-07-06 13:48:20 +1000

    feat: server hello world

---
+++ src/server.js
@@ -0,0 +1,17 @@
+
+const express = require ("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.json({
+        message:"Hello World"
+    });
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
